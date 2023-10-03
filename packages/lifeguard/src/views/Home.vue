8cf4c692a7ba1405145a6bc5aae1359a5f63faac
DecorteKasper <91284526+DecorteKasper@users.noreply.github.com>
2023-10-03 17:21:08 +0200

    feat: graphql added

--- packages/lifeguard/src/views/Home.vue
+++ packages/lifeguard/src/views/Home.vue
@@ -1,16 +1,54 @@
 <template>
-    <div>
-      <Container>
-        <h1 class="text-4xl font-bold tracking-wide mb-6">Dashboard</h1>
-  
-        <div>
-        </div>
-      </Container>
+  <Container>
+    <h1 class="text-4xl font-bold tracking-wide mb-6">Lifeguards</h1>
+
+    <div v-if="lifeguardsLoading">Loading...</div>
+    <div v-if="lifeguardsError">
+      {{ lifeguardsError }}
     </div>
-  </template>
-  
-  <script lang="ts">
-  import Container from '@/components/generic/Container.vue'
-  
-  export default { components: { Container } }
-  </script>
+
+    <div v-if="lifeguards" class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 grid-rows-auto gap-12">
+      <div v-for="lifeguard in lifeguards.lifeguards" class="flex items-center gap-6">
+        <img class="w-48" :src="`/birds/${lifeguard.name}.webp`" :alt="`Picture of ${lifeguard.name}.`" />
+        <div>
+          <h2 class="text-lg font-semibold tracking-wide">
+            {{ lifeguard.name }}
+          </h2>
+
+        </div>
+      </div>
+    </div>
+  </Container>
+</template>
+
+<script lang="ts">
+import { useQuery } from '@vue/apollo-composable'
+import Container from '@/components/generic/Container.vue'
+import { ALL_LIFEGUARDS } from '@/graphql/lifeguard.query'
+
+// TODO: refactor to interface
+interface Lifeguard {
+  id: string
+  name: string
+  surname: string
+}
+
+export default {
+  components: { Container },
+
+  setup() {
+    const {
+      loading: lifeguardsLoading,
+      result: lifeguards,
+      error: lifeguardsError,
+    } = useQuery(ALL_LIFEGUARDS)
+
+    return {
+      lifeguardsLoading,
+      lifeguards: lifeguards,
+      lifeguardsError,
+    }
+  },
+}
+</script>
+@/graphql/bird.query
