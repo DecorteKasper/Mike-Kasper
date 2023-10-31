237ef4bad66f4626e3480a7662e489e18291a930
Mike Vanoosthuyze <mike.vanoosthuyze@student.howest.be>
2023-10-31 15:58:55 +0100

    feature: navigation

--- packages/lifeguard/src/graphql/user.query.ts
+++ packages/lifeguard/src/graphql/user.query.ts
@@ -1,22 +1,41 @@
 import gql from 'graphql-tag'
 
 export const GET_USER_BY_UID = gql`
-  query userByUid($uid: String!) {
-    userByUid(string: $uid) {
-      id
-      uid
-      name
-      surname
-      email
-      phoneNumber
-      zipCode
-      street
-      numberOfHouse
-      birth
-      locale
-      role
-      createdAt
-      updatedAt
-    }
+query userByUid($uid: String!) {
+  userByUid(string: $uid) {
+    id
+    uid
+    name
+    surname
+    email
+    phoneNumber
+    zipCode
+    street
+    numberOfHouse
+    birth
+    locale
+    role
+    createdAt
+    updateAt
   }
+}
+`
+
+export const GET_USERS = gql` 
+query {
+  users {
+    uid
+    name
+    surname
+    role
+    email
+    bathingPlace
+    phoneNumber
+    zipCode
+    street
+    numberOfHouse
+    birth
+  }
+}
+
 `
