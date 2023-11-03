c5041805e5f8a06068386cc61f4163eaecd14692
Mike Vanoosthuyze <mike.vanoosthuyze@student.howest.be>
2023-11-03 23:45:06 +0100

    feat: table reports part 1

--- packages/lifeguard/src/components/generic/PrimaryButton.vue
+++ packages/lifeguard/src/components/generic/PrimaryButton.vue
@@ -1,18 +1,17 @@
 <template>
-    <button
-      class="mt-14 px-8 py-2 rounded-buttonRadius bg-green font-bold text-white hover:bg-dark_green focus:outline-none focus-visible:bg-dark_green"
-      @click="handleClick"
-    >
-      {{ label }}
-    </button>
-  </template>
+  <button
+    class="px-8 py-2 rounded-buttonRadius bg-green font-bold text-white hover:bg-dark_green focus:outline-none focus-visible:bg-dark_green"
+    @click="handleClick">
+    {{ label }}
+  </button>
+</template>
   
-  <script lang="ts">
-  import Vue from 'vue';
+<script lang="ts">
+import Vue from 'vue';
 
-  export default {
+export default {
 
-    props: {
+  props: {
     label: {
       type: String,
       required: true
@@ -24,8 +23,8 @@
     }
   }
 
-  }
-  
-  </script>
+}
+
+</script>
   
 
