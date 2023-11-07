ff15e9838bc827e62c1c73ae67235884e8378f8f
DecorteKasper <91284526+DecorteKasper@users.noreply.github.com>
2023-11-07 16:10:25 +0100

    Data console log werkt, nog beveiliging toevoegen

--- packages/lifeguard/src/views/redder/Report.vue
+++ packages/lifeguard/src/views/redder/Report.vue
@@ -81,21 +81,32 @@
                             :class="{'bg-greenx': isSelected('G'), 'bg-gray': !isSelected('G') }">
                         </v-checkbox>
                     </label>
+
+                    <div class="mt-6">
+                        <label for="vervanging">Vervanging?</label>
+                            <v-text-field 
+                                label="Naam vervanger"
+                                id="vervanging"
+                                class="mt-2"
+                                >
+                            </v-text-field>
+                    </div>
                 </div>
 
 
+<!-- Verloren gelopen kinderen -->
                 <div class="mt-16">
                         <div class="mb-5 mt-10">
                             <h2 class="font-bold">Verloren gelopen kinderen?</h2>
                             <div class="w-16 h-[2px] bg-redx rounded-full"></div>
                         </div>
-                        <V-radio-group v-model="formData.valueKind" :rules="formData.radioRules" required>
+                        <V-radio-group v-model="formData.valueRadioKind" :rules="formData.radioRules" required>
                             <label for="kind_1">
                                 <v-radio 
                                     label="Ja" 
                                     value="kind_1" 
                                     id="kind_1"
-                                    :class="{ 'bg-greenx': formData.valueKind === 'kind_1' }"
+                                    :class="{ 'bg-greenx': formData.valueRadioKind === 'kind_1' }"
                                     class="rounded-full h-13 text-black pl-3 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs">
                                 </v-radio>
                             </label>
@@ -104,38 +115,124 @@
                                     label="neen" 
                                     value="kind_0" 
                                     id="kind_0"
-                                    :class="{ 'bg-greenx': formData.valueKind === 'kind_0' }"
+                                    :class="{ 'bg-greenx': formData.valueRadioKind === 'kind_0' }"
                                     class="rounded-full h-13 text-black pl-3 mt-4 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs">
                                 </v-radio>
                             </label>
                         </V-radio-group>
                 </div>
 
+<!-- Interventies -->
                 <div class="mt-16">
                     <div class="mb-5 mt-10">
                         <h2 class="font-bold">Interventies?</h2>
                         <div class="w-16 h-[2px] bg-redx rounded-full"></div>
                     </div>
-                        <V-radio-group v-model="formData.valueInterventie" :rules="formData.radioRules" required>
+                         <v-radio-group v-model="formData.valueRadioInterventie" :rules="formData.radioRules" required>
                             <label for="interventie_1">
-                                <v-radio 
-                                    label="Ja" 
-                                    value="interventie_1" 
-                                    id="interventie_1"
-                                    :class="{ 'bg-greenx': formData.valueInterventie === 'interventie_1' }"
-                                    class="rounded-full h-13 text-black pl-3 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs">
+                                <v-radio
+                                label="Ja"
+                                value="interventie_1"
+                                id="interventie_1"
+                                @click="showAreaInterventie"
+                                :class="{ 'bg-greenx': formData.valueRadioInterventie === 'interventie_1' }"
+                                class="rounded-full h-13 text-black pl-3 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs"
+                                >
                                 </v-radio>
                             </label>
                             <label for="interventie_0">
-                                <v-radio 
-                                    label="neen" 
-                                    value="interventie_0" 
-                                    id="interventie_0"
-                                    :class="{ 'bg-greenx': formData.valueInterventie === 'interventie_0' }"
-                                    class="rounded-full h-13 text-black pl-3 mt-4 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs">
+                                <v-radio
+                                label="neen"
+                                value="interventie_0"
+                                id="interventie_0"
+                                @click="hideAreaInterventie"
+                                :class="{ 'bg-greenx': formData.valueRadioInterventie === 'interventie_0' }"
+                                class="rounded-full h-13 text-black pl-3 mt-4 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs"
+                                >
                                 </v-radio>
                             </label>
-                        </V-radio-group>
+                        </v-radio-group>
+
+                         <v-textarea
+                            v-if="showTextAreaFieldInterventie"
+                            label="Beschrijf de interventie(s)"
+                            v-model="formData.valueTextInterventie"
+                        ></v-textarea>
+                </div>
+
+<!-- Oefeningen uitgevoerd of gesport? -->
+                <div class="mt-16">
+                    <div class="mb-5 mt-10">
+                        <h2 class="font-bold">Oefeningen uitgevoerd of gesport?</h2>
+                        <div class="w-16 h-[2px] bg-redx rounded-full"></div>
+                    </div>
+                         <v-radio-group v-model="formData.valueRadioOefenen" :rules="formData.radioRules" required>
+                            <label for="oefeningOfSport_1">
+                                <v-radio
+                                label="Ja"
+                                value="oefeningOfSport_1"
+                                id="oefeningOfSport_1"
+                                @click="showAreaOefenenOfSport"
+                                :class="{ 'bg-greenx': formData.valueRadioOefenen === 'oefeningOfSport_1' }"
+                                class="rounded-full h-13 text-black pl-3 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs"
+                                >
+                                </v-radio>
+                            </label>
+                            <label for="oefeningOfSport_0">
+                                <v-radio
+                                label="neen"
+                                value="oefeningOfSport_0"
+                                id="oefeningOfSport_0"
+                                @click="hideAreaOefenenOfSport"
+                                :class="{ 'bg-greenx': formData.valueRadioOefenen === 'oefeningOfSport_0' }"
+                                class="rounded-full h-13 text-black pl-3 mt-4 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs"
+                                >
+                                </v-radio>
+                            </label>
+                        </v-radio-group>
+
+                         <v-textarea
+                            v-if="showTextAreaFieldOefenenOfSport"
+                            label="Beschrijf de oefening(en) of uitgevoerde sport(en)"
+                            v-model="formData.valueTextOefenen"
+                        ></v-textarea>
+                </div>
+
+                <div class="mt-16">
+                    <div class="mb-5 mt-10">
+                        <h2 class="font-bold">Materiaal kapot of te kort?</h2>
+                        <div class="w-16 h-[2px] bg-redx rounded-full"></div>
+                    </div>
+                         <v-radio-group v-model="formData.valueRadioMateriaal" :rules="formData.radioRules" required>
+                            <label for="materiaal_1">
+                                <v-radio
+                                label="Ja"
+                                value="materiaal_1"
+                                id="materiaal_1"
+                                @click="showAreaMateriaal"
+                                :class="{ 'bg-greenx': formData.valueRadioMateriaal === 'materiaal_1' }"
+                                class="rounded-full h-13 text-black pl-3 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs"
+                                >
+                                </v-radio>
+                            </label>
+                            <label for="materiaal_0">
+                                <v-radio
+                                label="neen"
+                                value="materiaal_0"
+                                id="materiaal_0"
+                                @click="hideAreaMateriaal"
+                                :class="{ 'bg-greenx': formData.valueRadioMateriaal === 'materiaal_0' }"
+                                class="rounded-full h-13 text-black pl-3 mt-4 bg-gray cursor-pointer max-w-[12rem] lg:max-w-xs"
+                                >
+                                </v-radio>
+                            </label>
+                        </v-radio-group>
+
+                         <v-textarea
+                            v-if="showTextAreaMateriaal"
+                            label="Wat is te kort of kapot?"
+                            v-model="formData.valueTextMateriaal"
+                        ></v-textarea>
                 </div>
 
                 <div class="mt-16">
@@ -143,7 +240,7 @@
                         <h2 class="font-bold">Andere info:</h2>
                         <div class="w-16 h-[2px] bg-redx rounded-full"></div>
                     </div>
-                    <v-textarea v-model="formData.valueInfo" label="Andere info"></v-textarea>
+                    <v-textarea v-model="formData.valueExtraInfo" label="Andere info"></v-textarea>
                 </div>
 
                  <v-btn color="primary" type="submit">Verslag verzenden</v-btn>
@@ -152,6 +249,8 @@
         </section>
     </Container>
 </template>
+
+
 
 <script lang="ts">
 import Container from '@/components/generic/Container.vue'
@@ -174,30 +273,34 @@ export default {
 
     data() {
         return {
-            // aanwezigen: ['A', 'C', 'D', 'F'],
             currentDate: new Date(),
-            // selectedValueKind: '',
-            // selectedValueInterventie: '',
-            // andereInfo: '',
             formData: {
                 aanwezigen: ['A', 'C', 'D', 'F'],
-                valueKind: '',
-                valueInterventie: '',
-                valueInfo: '',
+                valueRadioKind: '',
+                valueRadioInterventie: '',
+                valueTextInterventie: '',
+                valueRadioOefenen: '',
+                valueTextOefenen: '',
+                valueRadioMateriaal: '',
+                valueTextMateriaal: '',
+                valueExtraInfo: '',
                 radioRules: [
                     (v:any) => !!v || 'Selecteer een optie',
                 ],
-            }
+            },
+            showTextAreaFieldInterventie: false,
+            showTextAreaFieldOefenenOfSport: false,
+            showTextAreaMateriaal: false,
         }
 
     },
 
-    watch: {
-        selected(newVal) {
-            // This watcher will trigger when the 'selected' array changes
-            console.log("Selected items:", newVal);
-        }
-    },
+    // watch: {
+    //     selected(newVal) {
+    //         // This watcher will trigger when the 'selected' array changes
+    //         console.log("Selected items:", newVal);
+    //     }
+    // },
 
     methods: {
         isSelected(item:any) {
@@ -206,7 +309,30 @@ export default {
 
         submitForm() {
             console.log('Form data:', this.formData);
-        }
+        },
+
+        showAreaInterventie() {
+            this.showTextAreaFieldInterventie = true;
+        },
+        hideAreaInterventie() {
+            this.showTextAreaFieldInterventie = false;
+            this.formData.valueTextInterventie = '';
+        },
+        showAreaOefenenOfSport() {
+            this.showTextAreaFieldOefenenOfSport = true;
+        },
+        hideAreaOefenenOfSport() {
+            this.showTextAreaFieldOefenenOfSport = false;
+            this.formData.valueTextOefenen = '';
+        },
+        showAreaMateriaal() {
+            this.showTextAreaMateriaal = true;
+        },
+        hideAreaMateriaal() {
+            this.showTextAreaMateriaal = false;
+            this.formData.valueTextMateriaal = '';
+        },
+
         
 
         // logFormData() {
