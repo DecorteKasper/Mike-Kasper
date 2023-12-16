1ccd85894001cedbcd99f9e90a1b0a0fde782163
DecorteKasper <91284526+DecorteKasper@users.noreply.github.com>
2023-12-16 16:34:14 +0100

    checks resolver works

--- packages/api/src/checks/checks.resolver.spec.ts
+++ packages/api/src/checks/checks.resolver.spec.ts
@@ -1,9 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChecksResolver } from './checks.resolver';
-import { ChecksService } from './checks.service';
+import { checkStub } from './stubs/checks.stub';
+import { CreateCheckInput } from './dto/create-check.input';
+import { Check } from './entities/check.entity';
+
+import { ChecksService } from './checks.service'
+jest.mock('./checks.service');
+
+// "npx jest src/checks/checks.resolver.spec.ts" -> uitvoeren testen
 
 describe('ChecksResolver', () => {
   let resolver: ChecksResolver;
+  let mockedService: ChecksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +19,33 @@ describe('ChecksResolver', () => {
     }).compile();
 
     resolver = module.get<ChecksResolver>(ChecksResolver);
+    mockedService = module.get<ChecksService>(ChecksService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
+
+  //Create
+  describe('createCheck', () => {
+    let myCheckDTO: CreateCheckInput
+    let resultCheck: Check
+
+    beforeEach(async () => { 
+      myCheckDTO = {
+        checkMonths: checkStub().checkMonths,
+        checkHolidays: checkStub().checkHolidays,
+        accessPlatform: checkStub().accessPlatform
+      }
+      resultCheck = await resolver.createCheck(myCheckDTO)
+    })
+
+    it('should call checkService.create one time', async () => { 
+      expect(mockedService.create).toHaveBeenCalledTimes(1)
+    })
+
+    it('should call birdsService.create with the correct parameters', async () => {
+      expect(mockedService.create).toBeCalledWith(myCheckDTO)
+    })
+  })
 });
