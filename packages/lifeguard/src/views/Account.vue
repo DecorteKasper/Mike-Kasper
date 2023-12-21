5ea2e4f553f47364d17b6d7ad017a68518d754b5
DecorteKasper <91284526+DecorteKasper@users.noreply.github.com>
2023-12-21 17:29:13 +0100

    responsive

--- packages/lifeguard/src/views/Account.vue
+++ packages/lifeguard/src/views/Account.vue
@@ -1,6 +1,6 @@
 <template>
   <Container v-if="userAcces.userByUid.accessPlatform">
-    <div class="flex gap-40">
+    <div class="flex flex-col md:flex-row gap-4 md:gap-40">
       <ul class="shadow-cardShadow rounded-cardRadius p-10 h-fit flex flex-col gap-4">
         <h2 class="font-lato text-lg font-semibold mb-4">Instellingen</h2>
         <li @click="showForm(1)"
@@ -30,8 +30,7 @@
         <!-- <p> {{ firebaseUser }}</p> -->
         <!-- <p>{{ changeUser }}</p> -->
 
-
-        <div class="border-green border-b-2 mb-12">
+        <div class="border-greenx border-b-2 mb-12">
           <div>
             <img class="w-32 w-32 rounded-full bg-red mb-6" v-if="selectedFile" :src="imageUrl" alt="Uploaded Image" />
             <input id="fileInput" type="file" @change="handleFileUpload" class="hidden" />
@@ -48,99 +47,99 @@
         </div>
         <!-- Blok 2 -->
         <div class="border-greenx border-b-2 mb-12 ">
-          <h2 class="font-lato font-semibold text-lg mb-10">Persoonlijke gegevens</h2>
-          <div class="flex">
+          <h2 class="font-lato font-semibold text-lg mb-2">Persoonlijke gegevens</h2>
+          <div class="flex flex-col md:flex-row">
             <div class="mr-6">
-              <label for="name" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="name" class="text-sm font-lato block mt-6 mb-4 text-dark_grey2">
                 Voornaam </label>
               <input type="text" name="name" id="name" placeholder="Voornaam"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green"
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx"
                 v-model="changeUser.name" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
             <div class="mr-6">
-              <label for="surname" class="text-sm font-lato block mb-4 text-dark_grey2">
+               <label for="surname" class="text-sm mt-6 font-lato block mb-4 text-dark_grey2">
                 Achternaam
               </label>
               <input type="text" name="surname" id="surname" placeholder="naam" v-model="changeUser.surname"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
           </div>
-          <div class="flex mt-8">
+          <div class="flex mt-2 flex-col md:flex-row">
             <div class="mr-6">
-              <label for="email" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="email" class="text-sm font-lato mt-6 block mb-4 text-dark_grey2">
                 Email
               </label>
               <input type="text" name="email" id="email" placeholder="mike@gmail.com" v-model="changeUser.email"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
             <div class="mr-6">
-              <label for="tel" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="tel" class="text-sm font-lato mt-6 block mb-4 text-dark_grey2">
                 Telefoon
               </label>
               <input type="text" name="tel" id="tel" placeholder="+32 123 456 789" v-model="changeUser.phoneNumber"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
           </div>
-          <div class="flex mt-8 mb-12">
+          <div class="flex mt-2 mb-12 flex-col md:flex-row">
             <div class="mr-6">
-              <label for="birth" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="birth" class="text-sm font-lato block mt-6 mb-4 text-dark_grey2">
                 Geboortedatum
               </label>
               <input type="date" name="birth" id="birth" v-model="changeUser.birth"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
             <div class="mr-6">
-              <label for="birthplace" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="birthplace" class="text-sm font-lato block mt-6 mb-4 text-dark_grey2">
                 Geboorteplaats
               </label>
               <input type="text" name="birthplace" id="birthplace" placeholder="Knokke Heist"
                 v-model="changeUser.birthplace"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
           </div>
         </div>
         <!-- Blok 3 -->
         <div>
-          <h2 class="font-lato font-semibold text-lg mb-10">Woonplaats</h2>
-          <div class="flex">
+          <h2 class="font-lato font-semibold text-lg mb-2">Woonplaats</h2>
+          <div class="flex flex-col md:flex-row">
             <div class="mr-6">
-              <label for="street" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="street" class="text-sm font-lato block mt-6 mb-4 text-dark_grey2">
                 straat
               </label>
               <input type="text" name="street" id="street" placeholder="ruiterstraat" v-model="changeUser.street"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
             <div class="mr-6">
-              <label for="houseNumber" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="houseNumber" class="text-sm font-lato block mt-6 mb-4 text-dark_grey2">
                 huisnummer
               </label>
               <input type="text" name="houseNumber" id="houseNumber" placeholder="naam" v-model="changeUser.numberOfHouse"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
           </div>
-          <div class="flex mt-8 mb-12">
+          <div class="flex flex-col md:flex-row mt-2 mb-12">
             <div class="mr-6">
-              <label for="city" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="city" class="text-sm font-lato mt-6 block mb-4 text-dark_grey2">
                 Gemeente
               </label>
               <input type="text" name="city" id="city" placeholder="Knokke Heist" v-model="changeUser.city"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
             <div class="mr-6">
-              <label for="Zipcode" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="Zipcode" class="text-sm font-lato mt-6 block mb-4 text-dark_grey2">
                 Postcode
               </label>
               <input type="text" name="Zipcode" id="Zipcode" placeholder="8500" v-model="changeUser.zipCode"
-                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-green" />
+                class="mt-1 text-sm font-lato block w-full bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
             </div>
           </div>
@@ -150,10 +149,12 @@
         </div>
       </Form>
 
+
+
       <!-- Form 2 -->
       <Form v-if="selectedForm === 2" @submit.prevent="handleNewPassword" class="py-10">
         <div class="mb-10">
-          <h2 class="font-lato font-semibold text-lg mb-5">Wachtwoord instellen</h2>
+          <h2 class="font-lato font-semibold text-lg ">Wachtwoord instellen</h2>
           <!-- Feedback over opgeslagen wachtwoord -->
           <div :class="{
             'p-2 flex bg-dark_grey mb-5 rounded-inputFieldRadius w-fit transition-all ease-in duration-150': true,
@@ -164,10 +165,10 @@
             <CheckCircle class="ml-2 text-dark_green" :size="24" />
           </div>
 
-          <div class="flex">
+          <div class="flex flex-col md:flex-row -mt-2">
             <div class="mr-6">
               <!-- Hier komt input voor current password currentPassword -->
-              <label for="surname" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="surname" class="text-sm font-lato mt-6 block mb-4 text-dark_grey2">
                 Huidige wachtwoord
               </label>
               <!-- Hier komt input voor current password currentPassword -->
@@ -178,7 +179,7 @@
 
 
             <div class="mr-6">
-              <label for="surname" class="text-sm font-lato block mb-4 text-dark_grey2">
+              <label for="surname" class="text-sm font-lato mt-6 block mb-4 text-dark_grey2">
                 Nieuw wachtwoord
               </label>
               <!-- Hier komt input voor current password currentPassword -->
@@ -199,14 +200,14 @@
       <!-- Form 3 -->
       <Form @submit.prevent="handleDeleteAccount" v-if="selectedForm === 3" class="py-10">
         <div class="mb-10">
-          <h2 class="font-lato font-semibold text-lg mb-20">Account verwijderen</h2>
+          <h2 class="font-lato font-semibold text-lg mb-8">Account verwijderen</h2>
           <div class="flex">
             <div class="mr-6">
               <label for="name" class="text-sm font-lato block mb-4 text-dark_grey2">
-                Om het account te verwijderen, vul <strong>'verwijder'</strong> in het inputveld in.
+                Om het account te verwijderen, typ <strong>'verwijder'</strong>.
               </label>
               <input type="text" name="name" id="name" placeholder="verwijder" v-model="deleteString"
-                class="mt-1 text-sm font-lato block bg-dark_grey rounded-inputFieldRadius px-2 py-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
+                class="mt-1 text-sm font-lato block bg-dark_grey rounded-inputFieldRadius px-2 py-3 w-full focus:outline-none focus-visible:ring-2 focus-visible:ring-greenx" />
               <!-- <span class="text-red font-lato text-xs" v-if="v$.name.$error"> {{ v$.name.$errors[0].$message }}</span> -->
 
             </div>
